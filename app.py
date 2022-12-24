266599931022d40a210c86720e25c8cfb19672c4
mari <altysheva_maria@mail.ru>
2022-12-24 16:27:48 +0300

    db

--- app.py
+++ app.py
@@ -13,3 +13,22 @@ st.button('Создать отчет по продажам')
 st.button('Создать запрос на поставку')
 st.button('Создать отчет по закупкам')
 st.button('Выход')
+
+
+try:
+    sqlite_connection = sqlite3.connect('sqlite_python.db')
+    cursor = sqlite_connection.cursor()
+    print("База данных создана и успешно подключена к SQLite")
+
+    sqlite_select_query = "select sqlite_version();"
+    cursor.execute(sqlite_select_query)
+    record = cursor.fetchall()
+    print("Версия базы данных SQLite: ", record)
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Ошибка при подключении к sqlite", error)
+finally:
+    if (sqlite_connection):
+        sqlite_connection.close()
+        print("Соединение с SQLite закрыто")
---
+++ pages/Просмотр баз данных.py
@@ -0,0 +1,12 @@
+
+import io
+
+import streamlit as st
+
+
+st.button('Клиенты')
+st.button('Заказы клиентов')
+st.button('Склад')
+st.button('Поставщики')
+st.button('Заказы поставщикам')
+st.button('Назад')
