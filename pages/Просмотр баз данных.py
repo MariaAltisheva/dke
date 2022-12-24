f6675624b68efbba449180c7cb68c74b0f432972
mari <altysheva_maria@mail.ru>
2022-12-24 17:14:36 +0300

    db

--- pages/Просмотр баз данных.py
+++ pages/Просмотр баз данных.py
@@ -25,51 +25,109 @@
 # st.button('Назад')
 
 
-import sqlite3
-import pandas as pd
+# import sqlite3
+# import pandas as pd
+# import streamlit as st
+#
+#
+# try:
+#     sqlite_connection = sqlite3.connect('./db.sqlite3')
+#     cursor = sqlite_connection.cursor()
+# except sqlite3.Error as error:
+#     print("Ошибка при подключении к sqlite", error)
+#
+#
+# def clients():
+#     sqlite_select_query = "SELECT * from Клиент;"
+#     query = cursor.execute(sqlite_select_query)
+#     cols = [column[0] for column in query.description]
+#     results = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+#     st.table(data=results)
+#
+#
+# def goods():
+#     sqlite_select_query = "SELECT * from Товар;"
+#     query = cursor.execute(sqlite_select_query)
+#     cols = [column[0] for column in query.description]
+#     results = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+#     st.table(data=results)
+#
+#
+# def storage():
+#     sqlite_select_query = "SELECT * from Склад;"
+#     query = cursor.execute(sqlite_select_query)
+#     cols = [column[0] for column in query.description]
+#     results = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+#     st.table(data=results)
+#
+#
+# def orders():
+#     sqlite_select_query = "SELECT * from Заказ_клиента;"
+#     query = cursor.execute(sqlite_select_query)
+#     cols = [column[0] for column in query.description]
+#     results = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+#     st.table(data=results)
+#
+#
+# st.button('Просмотр товаров', on_click=goods)
+# st.button('Просмотр клиентов', on_click=clients)
+# st.button('Просмотр склада', on_click=storage)
+# st.button('Просмотр заказов', on_click=orders)
+
+
 import streamlit as st
+import pandas as pd
+import numpy as np
+import sqlite3 as lite
+
+st.title('Просмотр баз данных')
+
+def client():
+
+    conn = lite.connect('./db.sqlite3')
+    cur = conn.cursor()
+
+    df = pd.read_sql_query("SELECT * FROM Клиент", conn)
+    st.table(data=df)
 
 
-try:
-    sqlite_connection = sqlite3.connect('./db.sqlite3')
-    cursor = sqlite_connection.cursor()
-except sqlite3.Error as error:
-    print("Ошибка при подключении к sqlite", error)
+def client_order():
+
+    conn = lite.connect('./db.sqlite3')
+    cur = conn.cursor()
+
+    df = pd.read_sql_query("SELECT * FROM Заказ_клиента", conn)
+    st.table(data=df)
 
 
-def clients():
-    sqlite_select_query = "SELECT * from Клиент;"
-    query = cursor.execute(sqlite_select_query)
-    cols = [column[0] for column in query.description]
-    results = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
-    st.table(data=results)
+def warehouse():
+
+    conn = lite.connect('./db.sqlite3')
+    cur = conn.cursor()
+
+    df = pd.read_sql_query("SELECT * FROM Склад", conn)
+    st.table(data=df)
+
+def suppliers():
+
+    conn = lite.connect('./db.sqlite3')
+    cur = conn.cursor()
+
+    df = pd.read_sql_query("SELECT * FROM Поставщик", conn)
+    st.table(data=df)
+
+def suppliers_order():
+
+    conn = lite.connect('./db.sqlite3')
+    cur = conn.cursor()
+
+    df = pd.read_sql_query("SELECT * FROM Заказ_поставщика", conn)
+    st.table(data=df)
 
 
-def goods():
-    sqlite_select_query = "SELECT * from Товар;"
-    query = cursor.execute(sqlite_select_query)
-    cols = [column[0] for column in query.description]
-    results = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
-    st.table(data=results)
-
-
-def storage():
-    sqlite_select_query = "SELECT * from Склад;"
-    query = cursor.execute(sqlite_select_query)
-    cols = [column[0] for column in query.description]
-    results = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
-    st.table(data=results)
-
-
-def orders():
-    sqlite_select_query = "SELECT * from Заказ_клиента;"
-    query = cursor.execute(sqlite_select_query)
-    cols = [column[0] for column in query.description]
-    results = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
-    st.table(data=results)
-
-
-st.button('Просмотр товаров', on_click=goods)
-st.button('Просмотр клиентов', on_click=clients)
-st.button('Просмотр склада', on_click=storage)
-st.button('Просмотр заказов', on_click=orders)
+st.button('Клиенты', on_click=client)
+st.button('Заказы клиентов', on_click=client_order)
+st.button('Склад', on_click=warehouse)
+st.button('Поставщики', on_click=suppliers)
+st.button('Заказы поставщикам', on_click=suppliers_order)
+st.button('Назад')
